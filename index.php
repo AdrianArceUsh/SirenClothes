51511b75137a5d954584b19fd26e18aa0d7fafab
AnibalAdrianArce <anibal.adrian.arce@gmail.com>
2026-09-14 21:13:24 -0300

    Agrego el año a la pestaña

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Siren Clothes</title>
+  <title>Siren Clothes -2026</title>
 </head>
 <body>
 
